47bca2acb75ed433abc4fc67a07c430f6bda3208
Slaymish <hamishapps@gmail.com>
2021-09-11 13:22:16 +1200

    Add project files.

---
+++ Bettingtest/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace Bettingtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTeamA = new System.Windows.Forms.Button();
+            this.buttonTeamB = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBetAmount = new System.Windows.Forms.TextBox();
+            this.listBoxTeamA = new System.Windows.Forms.ListBox();
+            this.listBoxTeamB = new System.Windows.Forms.ListBox();
+            this.textBoxPrizepool = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxOdds = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonAWin = new System.Windows.Forms.Button();
+            this.buttonBWin = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTeamA
+            // 
+            this.buttonTeamA.Location = new System.Drawing.Point(50, 185);
+            this.buttonTeamA.Name = "buttonTeamA";
+            this.buttonTeamA.Size = new System.Drawing.Size(117, 37);
+            this.buttonTeamA.TabIndex = 3;
+            this.buttonTeamA.Text = "Bet Team A";
+            this.buttonTeamA.UseVisualStyleBackColor = true;
+            this.buttonTeamA.Click += new System.EventHandler(this.buttonTeamA_Click);
+            // 
+            // buttonTeamB
+            // 
+            this.buttonTeamB.Location = new System.Drawing.Point(50, 242);
+            this.buttonTeamB.Name = "buttonTeamB";
+            this.buttonTeamB.Size = new System.Drawing.Size(117, 37);
+            this.buttonTeamB.TabIndex = 4;
+            this.buttonTeamB.Text = "Bet Team B";
+            this.buttonTeamB.UseVisualStyleBackColor = true;
+            this.buttonTeamB.Click += new System.EventHandler(this.buttonTeamB_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(72, 94);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(82, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxBetAmount
+            // 
+            this.textBoxBetAmount.Location = new System.Drawing.Point(72, 139);
+            this.textBoxBetAmount.Name = "textBoxBetAmount";
+            this.textBoxBetAmount.Size = new System.Drawing.Size(82, 20);
+            this.textBoxBetAmount.TabIndex = 2;
+            // 
+            // listBoxTeamA
+            // 
+            this.listBoxTeamA.FormattingEnabled = true;
+            this.listBoxTeamA.Location = new System.Drawing.Point(265, 64);
+            this.listBoxTeamA.Name = "listBoxTeamA";
+            this.listBoxTeamA.Size = new System.Drawing.Size(104, 95);
+            this.listBoxTeamA.TabIndex = 0;
+            // 
+            // listBoxTeamB
+            // 
+            this.listBoxTeamB.FormattingEnabled = true;
+            this.listBoxTeamB.Location = new System.Drawing.Point(265, 197);
+            this.listBoxTeamB.Name = "listBoxTeamB";
+            this.listBoxTeamB.Size = new System.Drawing.Size(104, 95);
+            this.listBoxTeamB.TabIndex = 0;
+            // 
+            // textBoxPrizepool
+            // 
+            this.textBoxPrizepool.Location = new System.Drawing.Point(446, 185);
+            this.textBoxPrizepool.Name = "textBoxPrizepool";
+            this.textBoxPrizepool.ReadOnly = true;
+            this.textBoxPrizepool.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrizepool.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Bet: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 33);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Echo Better";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(212, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Team A:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Team B:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(384, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Prizepool: ";
+            // 
+            // textBoxOdds
+            // 
+            this.textBoxOdds.Location = new System.Drawing.Point(446, 142);
+            this.textBoxOdds.Name = "textBoxOdds";
+            this.textBoxOdds.ReadOnly = true;
+            this.textBoxOdds.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOdds.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(405, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Odds:";
+            // 
+            // buttonAWin
+            // 
+            this.buttonAWin.Location = new System.Drawing.Point(50, 345);
+            this.buttonAWin.Name = "buttonAWin";
+            this.buttonAWin.Size = new System.Drawing.Size(75, 23);
+            this.buttonAWin.TabIndex = 5;
+            this.buttonAWin.Text = "A Wins";
+            this.buttonAWin.UseVisualStyleBackColor = true;
+            this.buttonAWin.Click += new System.EventHandler(this.buttonAWin_Click);
+            // 
+            // buttonBWin
+            // 
+            this.buttonBWin.Location = new System.Drawing.Point(131, 345);
+            this.buttonBWin.Name = "buttonBWin";
+            this.buttonBWin.Size = new System.Drawing.Size(75, 23);
+            this.buttonBWin.TabIndex = 6;
+            this.buttonBWin.Text = "B Wins";
+            this.buttonBWin.UseVisualStyleBackColor = true;
+            this.buttonBWin.Click += new System.EventHandler(this.buttonBWin_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(50, 390);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(277, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(265, 345);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Clear All";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 619);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonBWin);
+            this.Controls.Add(this.buttonAWin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxOdds);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrizepool);
+            this.Controls.Add(this.listBoxTeamB);
+            this.Controls.Add(this.listBoxTeamA);
+            this.Controls.Add(this.textBoxBetAmount);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonTeamB);
+            this.Controls.Add(this.buttonTeamA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTeamA;
+        private System.Windows.Forms.Button buttonTeamB;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBetAmount;
+        private System.Windows.Forms.ListBox listBoxTeamA;
+        private System.Windows.Forms.ListBox listBoxTeamB;
+        private System.Windows.Forms.TextBox textBoxPrizepool;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxOdds;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonAWin;
+        private System.Windows.Forms.Button buttonBWin;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Bettingtest/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bettingtest
+{
+    public partial class Form1 : Form
+    {
+
+        // Declare Variables
+        decimal prizepool;
+        decimal teamATotal;
+        decimal teamBTotal;
+        decimal teamA;
+        decimal teamB;
+
+        // Declare lists
+        List<string> betterName = new List<string>();
+        List<char> betterTeam = new List<char>();
+        List<decimal> betterAmount = new List<decimal>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Bet(char team)
+        {
+            // Declare Variables
+            string name;
+            decimal betAmount;
+
+            try
+            {
+                // Get name and bet amount
+                name = textBoxName.Text;
+                betAmount = decimal.Parse(textBoxBetAmount.Text);
+
+                if (betAmount > 0)
+                {
+                    // Add vars to list
+                    betterName.Add(name);
+                    betterTeam.Add(team);
+                    betterAmount.Add(betAmount);
+
+
+                    Update_Lists();
+                    Odd_Prizepool();
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Enter an amount higher than 0");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Decide_Winners(char team)
+        {
+            string name;
+            decimal bet;
+            decimal payout;
+            decimal ratio;
+
+            listBox1.Items.Clear();
+
+            listBox1.Items.Add("Name" + "Initial".PadLeft(10) + "Payout".PadLeft(10));
+            bet = 0;
+            payout = 0;
+            ratio = 0;
+
+
+            for (int i = 0; i < betterAmount.Count; i++)
+            {
+                if (betterTeam[i] == team)
+                {
+                    bet = betterAmount[i];
+                    name = betterName[i];
+                    
+                    // Calculate payout
+                    if (team == 'A')
+                    {
+                        ratio = bet / teamA;
+                    }
+
+                    if (team == 'B')
+                    {
+                        ratio = bet / teamB;
+                    }
+
+                    payout = prizepool * ratio;
+
+
+                    listBox1.Items.Add(name + bet.ToString("c").PadLeft(10) + payout.ToString("c").PadLeft(10));
+                }
+                
+            }
+        }
+
+        private void buttonTeamA_Click(object sender, EventArgs e)
+        {
+            Create_Bet('A');
+        }
+
+        private void buttonTeamB_Click(object sender, EventArgs e)
+        {
+            Create_Bet('B');
+        }
+
+        private void Update_Lists()
+        {
+            // Update listboxes
+            char team;
+            string name;
+            decimal amount;
+
+            listBoxTeamA.Items.Clear();
+            listBoxTeamB.Items.Clear();
+
+            for (int i = 0; i < betterAmount.Count; i++)
+            {
+                team = betterTeam[i];
+                name = betterName[i];
+                amount = betterAmount[i];
+
+                if (team == 'A')
+                {
+                    listBoxTeamA.Items.Add(name + amount.ToString().PadLeft(10));
+                }
+                if (team == 'B')
+                {
+                    listBoxTeamB.Items.Add(name + amount.ToString().PadLeft(10));
+                }
+            }
+        }
+
+        private void Odd_Prizepool()
+        {
+            char team;
+            prizepool = 0;
+
+
+            teamA = 0;
+            teamB = 0;
+
+            // Calculate prizepool
+            for (int i = 0; i < betterAmount.Count; i++)
+            {
+                prizepool += betterAmount[i];
+                team = betterTeam[i];
+
+                if (team == 'A')
+                {
+                    teamA += betterAmount[i];
+                }
+                if (team == 'B')
+                {
+                    teamB += betterAmount[i];
+                }
+
+            }
+
+            // Calculate odds
+            textBoxOdds.Text = teamA.ToString() + " to " + teamB.ToString();
+
+            textBoxPrizepool.Text = prizepool.ToString("c");
+
+
+
+        }
+
+        private void buttonAWin_Click(object sender, EventArgs e)
+        {
+            Decide_Winners('A');
+        }
+
+        private void buttonBWin_Click(object sender, EventArgs e)
+        {
+            Decide_Winners('B');
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBoxBetAmount.Clear();
+            textBoxName.Clear();
+            listBoxTeamA.Items.Clear();
+            listBoxTeamB.Items.Clear();
+            textBoxOdds.Clear();
+            textBoxPrizepool.Clear();
+
+            betterAmount.Clear();
+            betterName.Clear();
+            betterTeam.Clear();
+        }
+    }
+}
